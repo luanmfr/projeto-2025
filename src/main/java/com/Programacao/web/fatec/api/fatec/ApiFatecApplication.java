21191b9cda6845dc6e8e8bbe2b6a1cf8e59e7da5
luan <luan.rodrigues9@fatec.sp.gov.br>
2025-02-19 19:31:08 -0300

    meu primeiro commit

---
+++ src/main/java/com/Programacao/web/fatec/api/fatec/ApiFatecApplication.java
@@ -0,0 +1,13 @@
+package com.Programacao.web.fatec.api.fatec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFatecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFatecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Programacao/web/fatec/api/fatec/ApiFatecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Programacao.web.fatec.api.fatec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFatecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
