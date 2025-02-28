2630677a64db092d000b9025696bd2b7aac520b6
luan <luan.rodrigues9@fatec.sp.gov.br>
2025-02-27 22:28:34 -0300

    adicinei função retronar idade e confuguerei o salvamento automatico , e função par impar

--- src/main/java/com/Programacao/web/fatec/api/fatec/conmtrolers/ExercicoControler.java
+++ src/main/java/com/Programacao/web/fatec/api/fatec/conmtrolers/ExercicoControler.java
@@ -1,17 +1,53 @@
 package com.Programacao.web.fatec.api.fatec.conmtrolers;
 
-import java.util.Optional;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 public class ExercicoControler {
-     @GetMapping("{nome}")
-    public String Heloword(@PathVariable Optional<String> nome){
-        return nome.isPresent() ? nome.get():"dd";
+
+    @GetMapping()
+    public String hello() {
+        return "GET";
     }
-    
-    
+
+    @GetMapping("/get-idade/{idade}")
+    public String retronaidade(@PathVariable Integer idade) {
+        try {
+            if (idade < 0) {
+                throw new NumberFormatException();
+            }
+            if (idade < 12) {
+                return "criança";
+            } else if (idade <= 18) {
+                return "adolecente";
+            } else if (idade >= 60) {
+                return "idoso";
+            } else {
+                return "adulto";
+            }
+
+        } catch (Exception e) {
+            return "idade invalida";
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "ExercicoControler []";
+    }
+    @GetMapping("/get-par/{par}")
+    public String retronaPar(@PathVariable Integer par){
+   
+        if (par % 2 == 0){
+            return "par";
+        }else{
+            return "impar";
+        }
+   
 }
+
+        }
+
